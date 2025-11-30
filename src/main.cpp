9c4a74d54f40fddeff9b2f9566feb5463635b049
LEXXES <lexixes@gmail.com>
2025-11-30 15:34:21 +0000

    2

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <drogon/drogon.h>
+#include <thread>
+#include <functional>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication app(argc, argv);
 
-    Server server;
 
-    return app.exec();
+    // Запускаем HTTP-сервер на Drogon в отдельном потоке
+    std::thread drogonThread([]() {
+        drogon::app()
+            .registerHandler(
+                "/ping",
+                [](const drogon::HttpRequestPtr &,
+                   std::function<void(const drogon::HttpResponsePtr &)> callback) {
+                    auto resp = drogon::HttpResponse::newHttpResponse();
+                    resp->setBody("pong");
+                    callback(resp);
+                })
+            .addListener("0.0.0.0", 8080)
+            .setThreadNum(1)
+            .run();
+    });
+    drogonThread.detach();
+
+
 }
 
