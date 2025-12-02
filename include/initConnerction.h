54519a4a931eddcd269e4cfac85a4882261126db
LEXXES <lexixes@gmail.com>
2025-12-02 20:26:44 +0000

    some cmake changes

--- include/initConnerction.h
+++ include/initConnerction.h
@@ -1,5 +1,7 @@
 #include <drogon/drogon.h>
 
+#define PORT 8080
+#define HOST "0.0.0.0"
 // Простая инициализация Drogon без конфигурационного файла.
 // При необходимости можно заменить на загрузку config.json:
 // drogon::app().loadConfigFile("config.json");
@@ -7,6 +9,6 @@ void initDrogon()
 {
     // Слушать на 0.0.0.0:8080
     drogon::app()
-        .addListener("0.0.0.0", 8080)
+        .addListener(HOST, PORT)
         .run();
 }
